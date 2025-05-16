bd3753cf58af3381a812191102d6fa19f6d4dea4
cosmicsundae <105429020+cosmicsundae@users.noreply.github.com>
2025-05-16 11:34:09 -0400

    Update check.js

--- apps/checkonline/check.js
+++ apps/checkonline/check.js
@@ -4,7 +4,6 @@ document.domain = "chaturbate.com"
 const options = {
   method: 'GET',
   headers: {
-    cookie: '__cf_bm=N4EXRhoaFU75IQ3kzQhrXFeYTQxX7Hs.3MssMRBOKPk-1669493664-0-AfSInvFUm5eb4gBsq2MDXMc8qjUiY3nvHKF2jLOqhZmAA4O2pl8vcbKFSgCQXkzhqh%2FVMiubaC9oAbzCdXYWZUY%3D; affkey=%22eJyrVipSslJQUqoFAAwfAk0%3D%22'
   }
 };
 
